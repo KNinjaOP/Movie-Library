87ba054926ce895c5e2c4f88c62f4f2366f2fbe9
KNinjaOP <eren9007op@gmail.com>
2025-11-29 21:01:19 +0530

    Updated API key usage and env config

--- src/sevices/api.js
+++ src/sevices/api.js
@@ -1,14 +1,12 @@
-const API_KEY = "9d3245943764b3577d17ec55cc457ce6";
+const API_KEY = import.meta.env.VITE_TMDB_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-  const data = await response.json()
-  return data.results;
+  const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  return (await res.json()).results;
 };
 
 export const searchMovies = async (query) => {
-  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
-  const data = await response.json()
-  return data.results;
+  const res = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+  return (await res.json()).results;
 };
